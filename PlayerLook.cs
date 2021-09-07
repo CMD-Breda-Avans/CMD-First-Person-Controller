97bf1e5196f29ae346b426cd5a4e8a3d15a0a48d
Hans van Arken <hansvanarken@gmail.com>
2021-09-07 15:46:33 +0200

    Update PlayerLook.cs
    Locking cursor in game view

--- PlayerLook.cs
+++ PlayerLook.cs
@@ -19,10 +19,11 @@ public class PlayerLook : MonoBehaviour
     }
     void Update()
     {
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
-        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
-        transform.eulerAngles = new Vector2(0,rotation.y) * lookSpeed;
-        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+      Cursor.lockState = CursorLockMode.Locked;
+      rotation.y += Input.GetAxis("Mouse X");
+      rotation.x += -Input.GetAxis("Mouse Y");
+      rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+      transform.eulerAngles = new Vector2(0,rotation.y) * lookSpeed;
+      Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
     }
 }
